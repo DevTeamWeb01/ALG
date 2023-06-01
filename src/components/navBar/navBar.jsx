15655c07cf668c27476636f258bc348ce52c2307
Gabriel Martinez <martinegabriel47@gmail.com>
2023-06-01 10:29:31 -0500

    update menu

--- src/components/navBar/navBar.jsx
+++ src/components/navBar/navBar.jsx
@@ -1,17 +1,113 @@
-import "../../styles/components/navBar.css";
+import { useState } from 'react';
+
 const NavBar = () => {
+  const [visible, setVisible] = useState(false);
+  const [subMenuVisible, setSubMenuVisible] = useState(false);
+
+  const toggleMenu = () => {
+    setVisible(!visible);
+  };
+
+  const subMenu = () => {
+    setSubMenuVisible(!subMenuVisible);
+  };
+
+  const stopPropagation = (event) => {
+    event.stopPropagation();
+  };
+
   return (
-    <>
-      <div className="bg-[#35C164] p-10 flex-col w-14 h-[100vh] items-center flex py-5 font-mono">
-        <h2 className="text-white text-2xl pb-9">ALG</h2>
-        <box-icon color="#fff" name="menu"></box-icon>
-        <nav className="py-5 flex flex-col items-center text-yellow-400">
-          <li className="flex flex-col items-center py-2 gap-2 cursor-pointer p-5 hover:bg-[rgba(255,255,255,0.5)]">
-            <box-icon color="#fff" name="dashboard" type="solid"></box-icon>
-          </li>
-        </nav>
+    <div className="bg-[#35C164] fixed w-[260px] h-[100vh] font-mono">
+      <div className="h-[60px] w-[100%] flex flex-col items-center">
+        <h1 className="text-white text-2xl">ALG</h1>
+        <span className="cursor-pointer  hover:bg-[rgba(255,255,255,0.2)]" onClick={toggleMenu}>
+          <box-icon color="#fff" name="menu"></box-icon>
+        </span>
       </div>
-    </>
+      {visible && (
+        <nav className="pt-8">
+          <ul className='flex flex-col'>
+            <li className='relative cursor-pointer'>
+              <a className='flex items-center'>
+                <span className='h-[40px] min-w-[40px] leading-[50px] '>
+                  <box-icon name='dashboard' type='solid' color='#ffffff' ></box-icon>
+                </span>
+                <span>Inicio</span>
+              </a>
+            </li>
+
+            <li className='relative cursor-pointer pt-5' onClick={subMenu}>
+              <div className='flex items-center justify-around'>
+                <a className='flex items-center'>
+                  <span className='h-[40px] min-w-[40px] leading-[50px] '>
+                    <box-icon name='dashboard' type='solid' color='#ffffff' ></box-icon>
+                  </span>
+                  <span className=''>Ingreso general</span>
+                </a>
+                <box-icon name='chevron-down' color='#ffffff' ></box-icon>
+              </div>
+              {subMenuVisible && (
+                <ul className='pt-[6px] pr-[10px] pb-[14px] pl-[30px] flex flex-col bg-[#278d49] rounded-md' onClick={stopPropagation}>
+                  <li className='cursor-pointer p-2'>
+                    <a className=''>
+                      <span className='pr-2'>
+                        <box-icon name='user' color='#ffffff' ></box-icon>
+                      </span>
+                      <span>Clientes</span>
+                    </a>
+                  </li>
+
+                  <li className='cursor-pointer p-2'>
+                    <a className=''>
+                      <span className='pr-2'>
+                        <box-icon name='user' color='#ffffff' ></box-icon>
+                      </span>
+                      <span>Proveedores</span>
+                    </a>
+                  </li>
+
+                  <li className='cursor-pointer p-2'>
+                    <a className=''>
+                      <span className='pr-2'>
+                        <box-icon name='user' color='#ffffff' ></box-icon>
+                      </span>
+                      <span>Personal</span>
+                    </a>
+                  </li>
+
+                  <li className='cursor-pointer p-2'>
+                    <a className=''>
+                      <span className='pr-2'>
+                        <box-icon name='user' color='#ffffff' ></box-icon>
+                      </span>
+                      <span>Clientes</span>
+                    </a>
+                  </li>
+                </ul>
+              )}
+            </li>
+
+            <li className='relative cursor-pointer pt-5'>
+              <a className='flex items-center'>
+                <span className='h-[40px] min-w-[40px] leading-[50px] '>
+                  <box-icon name='dashboard' type='solid' color='#ffffff' ></box-icon>
+                </span>
+                <span>Obras en ejecucion</span>
+              </a>
+            </li>
+
+            <li className='relative cursor-pointer pt-5'>
+              <a className='flex items-center'>
+                <span className='h-[40px] min-w-[40px] leading-[50px] '>
+                  <box-icon name='dashboard' type='solid' color='#ffffff' ></box-icon>
+                </span>
+                <span>Preosupuesto</span>
+              </a>
+            </li>
+          </ul>
+        </nav>
+      )}
+    </div>
   );
 };
 
