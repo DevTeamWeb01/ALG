7fba1de770a31e40f5b6a439f03f0a325b4c46d8
Andres Camacho <99270915+af01camacho@users.noreply.github.com>
2023-06-01 12:54:44 -0500

    update navbar

--- src/components/navBar/navBar.jsx
+++ src/components/navBar/navBar.jsx
@@ -1,5 +1,5 @@
-import { useState } from 'react';
-
+import { useState } from "react";
+import logo from "../../assets/imagen1.png";
 const NavBar = () => {
   const [visible, setVisible] = useState(false);
   const [subMenuVisible, setSubMenuVisible] = useState(false);
@@ -11,102 +11,107 @@ const NavBar = () => {
   const subMenu = () => {
     setSubMenuVisible(!subMenuVisible);
   };
-
-  const stopPropagation = (event) => {
-    event.stopPropagation();
-  };
-
   return (
-    <div className="bg-[#35C164] fixed w-[260px] h-[100vh] font-mono">
-      <div className="h-[60px] w-[100%] flex flex-col items-center">
-        <h1 className="text-white text-2xl">ALG</h1>
-        <span className="cursor-pointer  hover:bg-[rgba(255,255,255,0.2)]" onClick={toggleMenu}>
-          <box-icon color="#fff" name="menu"></box-icon>
-        </span>
-      </div>
-      {visible && (
-        <nav className="pt-8">
-          <ul className='flex flex-col'>
-            <li className='relative cursor-pointer'>
-              <a className='flex items-center'>
-                <span className='h-[40px] min-w-[40px] leading-[50px] '>
-                  <box-icon name='dashboard' type='solid' color='#ffffff' ></box-icon>
-                </span>
-                <span>Inicio</span>
-              </a>
-            </li>
-
-            <li className='relative cursor-pointer pt-5' onClick={subMenu}>
-              <div className='flex items-center justify-around'>
-                <a className='flex items-center'>
-                  <span className='h-[40px] min-w-[40px] leading-[50px] '>
-                    <box-icon name='dashboard' type='solid' color='#ffffff' ></box-icon>
+    <div className="bg-[#f4f4f4] flex">
+      {/* Container */}
+      <div className=" w-[70px] md:w-[200px] h-screen bg-white p-1 flex flex-col text-xxl gap-2">
+        {/* Container / Logo */}
+        <figure>
+          <img src={logo} alt="Aire Limpio Global" className="w-3/4 flex" />
+        </figure>
+        {/* Container / Menu */}
+        <div className="flex flex-col   gap-2 transition-all duration-1000 delay-700">
+          <div className="flex justify-center">
+            <span>MENU</span>
+            <div className="absolute left-[63px] rotate-180 flex bg-[#2fb71d85] rounded md:left-[188px]">
+              <box-icon name="chevron-right"></box-icon>
+            </div>
+          </div>
+          <nav>
+            <ul className="flex flex-col gap-2 md:items-start items-center">
+              <li className=" flex justify-center md:justify-between hover:bg-[#2fb71d85] hover:text-[#052c02] p-2 relative w-[100%] hover:after:content-['_.'] after:bg-black after:h-[100%]">
+                <a className="flex items-center gap-2 " href="#">
+                  <box-icon type="solid" name="dashboard"></box-icon>
+                  <span className="absolute left-[-900px] md:relative md:left-0 transition-all duration-40 delay-0 ">
+                    Dashboard
                   </span>
-                  <span className=''>Ingreso general</span>
                 </a>
-                <box-icon name='chevron-down' color='#ffffff' ></box-icon>
-              </div>
-              {subMenuVisible && (
-                <ul className='pt-[6px] pr-[10px] pb-[14px] pl-[30px] flex flex-col bg-[#278d49] rounded-md' onClick={stopPropagation}>
-                  <li className='cursor-pointer p-2'>
-                    <a className=''>
-                      <span className='pr-2'>
-                        <box-icon name='user' color='#ffffff' ></box-icon>
-                      </span>
-                      <span>Clientes</span>
-                    </a>
-                  </li>
+              </li>
+              <li className="flex justify-center md:justify-between hover:bg-[#2fb71d85] hover:text-[#052c02] p-2 relative w-[100%] hover:after:content-['_.'] after:bg-black after:h-[100%]">
+                <a className="flex items-center gap-2" href="">
+                  <box-icon type="solid" name="report"></box-icon>
+                  <span className="absolute left-[-900px] md:relative md:left-0 transition-all duration-40 delay-0">
+                    Reportes
+                  </span>
+                </a>
+              </li>
+              <li className="flex justify-center md:justify-between hover:bg-[#2fb71d85] hover:text-[#052c02] p-2 relative w-[100%] hover:after:content-['_.'] after:bg-black after:h-[100%]">
+                <a className="flex items-center gap-2" href="">
+                  <box-icon type="solid" name="briefcase-alt"></box-icon>
+                  <span className="absolute left-[-900px] md:relative md:left-0 transition-all duration-40 delay-0">
+                    Ingresos
+                  </span>
+                </a>
+              </li>
+              <ul className=" flex flex-col w-full">
+                <li className="justify-between hover:bg-[#2fb71d85] hover:text-[#052c02] p-2">
+                  <a href="#">Clientes</a>
+                </li>
+                <li className="justify-between hover:bg-[#2fb71d85] hover:text-[#052c02] p-2">
+                  <a href="#">Proveedores</a>
+                </li>
+                <li className="justify-between hover:bg-[#2fb71d85] hover:text-[#052c02] p-2">
+                  <a href="#">Personal</a>
+                </li>
+              </ul>
+              <li className="flex justify-center md:justify-between hover:bg-[#2fb71d85] hover:text-[#052c02] p-2 relative w-[100%] hover:after:content-['_.'] after:bg-black after:h-[100%]">
+                <a className="flex items-center gap-2" href="">
+                  <box-icon name="task"></box-icon>
+                  <span className="absolute left-[-900px] md:relative md:left-0 transition-all duration-40 delay-0">
+                    Obras en ejecucion
+                  </span>
+                </a>
+              </li>
+              <li className="flex justify-center md:justify-between hover:bg-[#2fb71d85] hover:text-[#052c02] p-2 relative w-[100%] hover:after:content-['_.'] after:bg-black after:h-[100%]">
+                <a className="flex items-center gap-2" href="">
+                  <box-icon name="money"></box-icon>
+                  <span className="absolute left-[-900px] md:relative md:left-0 transition-all duration-40 delay-0">
+                    Presupuesto
+                  </span>
+                </a>
+              </li>
+              <li className="flex justify-center md:justify-between hover:bg-[#2fb71d85] hover:text-[#052c02] p-2 relative w-[100%] hover:after:content-['_.'] after:bg-black after:h-[100%]">
+                <a className="flex items-center gap-2" href="">
+                  <box-icon type="solid" name="user-voice"></box-icon>
+                  <span className="absolute left-[-900px] md:relative md:left-0 transition-all duration-40 delay-0">
+                    Vendedores
+                  </span>
+                </a>
+              </li>
+              <li className="flex justify-center md:justify-between hover:bg-[#2fb71d85] hover:text-[#052c02] p-2 relative w-[100%] hover:after:content-['_.'] after:bg-black after:h-[100%]">
+                <a className="flex items-center gap-2" href="">
+                  <box-icon name="box"></box-icon>
+                  <span className="absolute left-[-900px] md:relative md:left-0 transition-all duration-40 delay-0">
+                    Inventario
+                  </span>
+                </a>
+              </li>
+              <li className="flex justify-center md:justify-between hover:bg-[#2fb71d85] hover:text-[#052c02] p-2 relative w-[100%] hover:after:content-['_.'] after:bg-black after:h-[100%]">
+                <a className="flex items-center gap-2" href="">
+                  <box-icon name="money"></box-icon>
+                  <span className="absolute left-[-900px] md:relative md:left-0 transition-all duration-40 delay-0">
+                    Contabilidad
+                  </span>
+                </a>
+              </li>
+            </ul>
+          </nav>
+        </div>
+      </div>
 
-                  <li className='cursor-pointer p-2'>
-                    <a className=''>
-                      <span className='pr-2'>
-                        <box-icon name='user' color='#ffffff' ></box-icon>
-                      </span>
-                      <span>Proveedores</span>
-                    </a>
-                  </li>
-
-                  <li className='cursor-pointer p-2'>
-                    <a className=''>
-                      <span className='pr-2'>
-                        <box-icon name='user' color='#ffffff' ></box-icon>
-                      </span>
-                      <span>Personal</span>
-                    </a>
-                  </li>
-
-                  <li className='cursor-pointer p-2'>
-                    <a className=''>
-                      <span className='pr-2'>
-                        <box-icon name='user' color='#ffffff' ></box-icon>
-                      </span>
-                      <span>Clientes</span>
-                    </a>
-                  </li>
-                </ul>
-              )}
-            </li>
-
-            <li className='relative cursor-pointer pt-5'>
-              <a className='flex items-center'>
-                <span className='h-[40px] min-w-[40px] leading-[50px] '>
-                  <box-icon name='dashboard' type='solid' color='#ffffff' ></box-icon>
-                </span>
-                <span>Obras en ejecucion</span>
-              </a>
-            </li>
-
-            <li className='relative cursor-pointer pt-5'>
-              <a className='flex items-center'>
-                <span className='h-[40px] min-w-[40px] leading-[50px] '>
-                  <box-icon name='dashboard' type='solid' color='#ffffff' ></box-icon>
-                </span>
-                <span>Preosupuesto</span>
-              </a>
-            </li>
-          </ul>
-        </nav>
-      )}
+      <div>
+        <h2>asds</h2>
+      </div>
     </div>
   );
 };
